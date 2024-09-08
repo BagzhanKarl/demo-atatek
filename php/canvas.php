cb5ffe6c9f2fc1cc4b54e86cfa17cfc13b6415da
newri <bagzhankarl@gmail.com>
2024-09-08 07:52:30 +0600

    First test on server

--- php/canvas.php
+++ php/canvas.php
@@ -6,36 +6,42 @@ $info = R::findOne('info', 'item_id = ?', [$_POST['id']]);
 ?>
 <div class="mb-2 d-flex justify-content-between align-items-center">
     <div class="text-block">
-        <div class="name"><?= $tree->name?></div>
-        <div class="date">
-            <?php
-            if($tree->birth_year != 0){
-                echo $info->birth_year . " ";
-            }
-            if($tree->death_year != 0){
-                echo $info->death_year;
-            }
-            ?>
-        </div>
+        <?php if ($tree): ?>
+            <div class="name"><?= $tree->name ?></div>
+            <div class="date">
+                <?php
+                if ($tree->birth_year != 0 && $info) {
+                    echo $info->birth_year . " ";
+                }
+                if ($tree->death_year != 0 && $info) {
+                    echo $info->death_year;
+                }
+                ?>
+            </div>
+        <?php else: ?>
+            <div class="name">Дерево не найдено</div>
+        <?php endif; ?>
     </div>
     <div class="img-block">
         <img src="images/flag-outline%201.png" alt="">
     </div>
 </div>
-<?php if($info):?>
+
+<?php if ($info): ?>
     <div class="mb-3" style="overflow-y: scroll; height: 300px;">
-        <? echo nl2br($info->text)?>
+        <?= nl2br($info->text) ?>
     </div>
-<?php else:?>
+<?php else: ?>
     <div class="mb-3" style="overflow-y: scroll; height: 300px;">
         Біз бұл адам туралы ақпаратты таппадық!
     </div>
-<?php endif;?>
+<?php endif; ?>
+
 <div class="mt-auto">
     <div class="d-flex gap-3">
         <button onclick="$('#canvasAta').hide();" class="btn btn-outline-success w-100">Түсінікті</button>
-        <?php if($user->admin == 1):?>
-            <a target="_blank" href="admin/addinfo.php?id=<?= $_POST['id']?>" class="btn btn-success w-100">Өзгерту</a>
-        <?php endif;?>
+        <?php if ($user && $user->admin == 1): ?>
+            <a target="_blank" href="admin/addinfo.php?id=<?= $_POST['id'] ?>" class="btn btn-success w-100">Өзгерту</a>
+        <?php endif; ?>
     </div>
 </div>
